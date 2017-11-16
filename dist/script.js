44f6203a78a979932e145b6686679a341c3e9803
LukeWlodarczyk <wlod_a_rczyk@interia.pl>
2017-11-16 12:40:43 +0100

    DomContentLoaded

--- dist/script.js
+++ dist/script.js
@@ -70,111 +70,115 @@
 "use strict";
 
 
-var ufos = document.querySelectorAll('.ufo');
-var cows = document.querySelectorAll('.cow');
-var wrappers = document.querySelectorAll('.wrapper');
-var startBtn = document.querySelector('button');
-var easyBtn = document.querySelector('#easy');
-var mediumBtn = document.querySelector('#medium');
-var hardBtn = document.querySelector('#hard');
-var scoreBox = document.querySelector('.score');
-var minNumAnimals = 3;
-var abducted = [];
-var lastCow = void 0;
-var score = 0;
-var bestScore = 0;
-var rndNum = void 0;
-var interval = null;
-easyBtn.addEventListener('click', easyLevel);
-mediumBtn.addEventListener('click', mediumLevel);
-hardBtn.addEventListener('click', hardLevel);
-startBtn.addEventListener('click', function () {
-  startAbduction();
-});
-
-function easyLevel() {
-  wrappers.forEach(function (w) {
-    w.classList.remove('medium');
-    w.classList.remove('hard');
+document.addEventListener("DOMContentLoaded", function (event) {
+  var ufos = document.querySelectorAll('.ufo');
+  var cows = document.querySelectorAll('.cow');
+  var wrappers = document.querySelectorAll('.wrapper');
+  var startBtn = document.querySelector('button');
+  var easyBtn = document.querySelector('#easy');
+  var mediumBtn = document.querySelector('#medium');
+  var hardBtn = document.querySelector('#hard');
+  var scoreBox = document.querySelector('.score');
+  var minNumAnimals = 3;
+  var abducted = [];
+  var lastCow = void 0;
+  var score = 0;
+  var bestScore = 0;
+  var rndNum = void 0;
+  var interval = null;
+  easyBtn.addEventListener('click', easyLevel);
+  mediumBtn.addEventListener('click', mediumLevel);
+  hardBtn.addEventListener('click', hardLevel);
+  startBtn.addEventListener('click', function () {
+    startAbduction();
   });
-  bestScore = 0;
-  minNumAnimals = 3;
-  startAbduction();
-}
 
-function mediumLevel() {
-  wrappers.forEach(function (w) {
-    w.classList.remove('hard');
-    w.classList.add('medium');
-  });
-  bestScore = 0;
-  minNumAnimals = 4;
-  startAbduction();
-}
-
-function hardLevel() {
-  wrappers.forEach(function (w) {
-    w.classList.add('hard');
-  });
-  bestScore = 0;
-  minNumAnimals = 5;
-  startAbduction();
-}
-
-function startAbduction() {
-  clearInterval(interval);
-  wrappers.forEach(function (w) {
-    w.classList.remove('abduction');
-  });
-  cows.forEach(function (cow) {
-    cow.classList.remove('visible');
-  });
-  cows.forEach(function (cow) {
-    cow.classList.add('visible');
-  });
-  abducted = [];
-  score = 0;
-  scoreBox.textContent = 'Min animals: ' + minNumAnimals + ' / Score: ' + score + ' / Best: ' + bestScore;
-
-  interval = setInterval(abduct, 1300);
-  var timeout = null;
-  function abduct() {
-    if (abducted.length === cows.length - minNumAnimals) {
-      console.log('clear');
-      clearInterval(interval);
-      return;
-    }
-
-    rndNum = Math.floor(Math.random() * cows.length);
-    if (abducted.includes(rndNum) || rndNum === lastCow) {
-      console.log('include');
-      abduct();
-      return;
-    }
-    lastCow = rndNum;
-    cows[rndNum].parentElement.classList.add('abduction');
-    console.log('addClass');
-    abducted.push(rndNum);
+  function easyLevel() {
+    wrappers.forEach(function (w) {
+      w.classList.remove('medium');
+      w.classList.remove('hard');
+    });
+    bestScore = 0;
+    minNumAnimals = 3;
+    startAbduction();
   }
-}
 
-ufos.forEach(function (ufo) {
-  return ufo.addEventListener('click', shoot);
+  function mediumLevel() {
+    wrappers.forEach(function (w) {
+      w.classList.remove('hard');
+      w.classList.add('medium');
+    });
+    bestScore = 0;
+    minNumAnimals = 4;
+    startAbduction();
+  }
+
+  function hardLevel() {
+    wrappers.forEach(function (w) {
+      w.classList.add('hard');
+    });
+    bestScore = 0;
+    minNumAnimals = 5;
+    startAbduction();
+  }
+
+  function startAbduction() {
+    clearInterval(interval);
+    wrappers.forEach(function (w) {
+      w.classList.remove('abduction');
+    });
+    cows.forEach(function (cow) {
+      cow.classList.remove('visible');
+    });
+    cows.forEach(function (cow) {
+      cow.classList.add('visible');
+    });
+    abducted = [];
+    score = 0;
+    scoreBox.textContent = 'Min animals: ' + minNumAnimals + ' / Score: ' + score + ' / Best: ' + bestScore;
+
+    interval = setInterval(abduct, 1300);
+    var timeout = null;
+
+    function abduct() {
+      if (abducted.length === cows.length - minNumAnimals) {
+        console.log('clear');
+        clearInterval(interval);
+        return;
+      }
+
+      rndNum = Math.floor(Math.random() * cows.length);
+      if (abducted.includes(rndNum) || rndNum === lastCow) {
+        console.log('include');
+        abduct();
+        return;
+      }
+      lastCow = rndNum;
+      cows[rndNum].parentElement.classList.add('abduction');
+      console.log('addClass');
+      abducted.push(rndNum);
+    }
+  }
+
+  ufos.forEach(function (ufo) {
+    return ufo.addEventListener('click', shoot);
+  });
+
+  function shoot() {
+    var _this = this;
+
+    score++;
+    score > bestScore ? bestScore = score : bestScore = bestScore;
+    scoreBox.textContent = 'Min animals: ' + minNumAnimals + ' / Score: ' + score + ' / Best: ' + bestScore;
+    this.parentElement.classList.remove('abduction');
+    this.classList.add('hit');
+    var index = abducted.indexOf(rndNum);
+    abducted.splice(index, 1);
+    setTimeout(function () {
+      _this.classList.remove('hit');
+    }, 650);
+  }
 });
-function shoot() {
-  var _this = this;
-
-  score++;
-  score > bestScore ? bestScore = score : bestScore = bestScore;
-  scoreBox.textContent = 'Min animals: ' + minNumAnimals + ' / Score: ' + score + ' / Best: ' + bestScore;
-  this.parentElement.classList.remove('abduction');
-  this.classList.add('hit');
-  var index = abducted.indexOf(rndNum);
-  abducted.splice(index, 1);
-  setTimeout(function () {
-    _this.classList.remove('hit');
-  }, 650);
-}
 
 /***/ })
 /******/ ]);
